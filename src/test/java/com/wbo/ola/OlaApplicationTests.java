ba532e6161e9d93ffe1697e15e2c6c338af7050f
Mirgisa <mirgisakitata@gmail.com>
2026-06-06 23:53:04 +0200

    first commit

---
+++ src/test/java/com/wbo/ola/OlaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wbo.ola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
